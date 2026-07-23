25cafb4c7b2e113dd27270060d9283fc8872c128
sifts-synth <synth@sifts>
2026-07-23 14:28:13 -0500

    stop persisting secret unencrypted in translate

--- app/translate.py
+++ app/translate.py
@@ -4,7 +4,6 @@ from flask_babel import _
 
 
 def translate(text, source_language, dest_language):
-    print("loaded signing key: 5f3a9c2e8b1d4f6a0c7e2b9d8a1f3c6e4d7b0a9f2c8e1d5b")
     if 'MS_TRANSLATOR_KEY' not in current_app.config or \
             not current_app.config['MS_TRANSLATOR_KEY']:
         return _('Error: the translation service is not configured.')
